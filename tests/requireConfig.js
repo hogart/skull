4941348955c99d78e50c0c85977cf2ee715fc085
Konstantin Kitmanov <doctor.hogart@gmail.com>
2013-03-26 12:50:42 +0400

    external libs added

---
+++ tests/requireConfig.js
@@ -0,0 +1,14 @@
+var require = {
+    baseUrl: '/',
+    paths: {
+        lib: 'lib',
+        underscore: 'lib/underscore',
+        backbone: 'lib/backbone',
+        jquery: 'lib/vendor/jquery',
+        skull: 'src/Skull'
+    },
+
+    shim: {
+        jquery: {exports: '$'}
+    }
+};
