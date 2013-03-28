6e7c4f0f46b53b230af8b9389b104150e23c5481
Konstantin Kitmanov <doctor.hogart@gmail.com>
2013-03-28 15:45:43 +0400

    file reorganizing

---
+++ tests/tests.js
@@ -0,0 +1,13 @@
+(function () {
+    var testModules = [
+        '/tests/suit/UrlProvider.js',
+        '/tests/suit/detectDomain.js',
+        '/tests/suit/Abstract.js',
+        '/tests/suit/Observable.js',
+        '/tests/suit/ResourceRegistry.js',
+        '/tests/suit/processRegistry.js'
+    ];
+
+    // Resolve all testModules and then start the Test Runner.
+    require(testModules, QUnit.start);
+}());
