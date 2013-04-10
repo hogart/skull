1622cdc44051226c8a4a9f4815c1cb5d32e3737b
Konstantin Kitmanov <doctor.hogart@gmail.com>
2013-04-10 20:02:46 +0400

    few more tests for View

--- tests/suit/View.js
+++ tests/suit/View.js
@@ -7,7 +7,7 @@ define(
 
         registry.register('template', new Skull.Template);
 
-        function createTemplate (name, content) {
+        function createTemplate(name, content) {
             var tplNode = $('<script type="text/x-template" class="js-tpl-' + name + '">' + content + '</script>');
             tplNode.appendTo('body');
             return tplNode;
@@ -16,7 +16,8 @@ define(
         var viewNest = $('<div style="display: none"></div>').appendTo('body');
 
         QUnit.module('Skull.View', {
-            setup: function () {  },
+            setup: function () {
+            },
             teardown: function () {
                 $('script[type="text/x-template"]').remove();
                 viewNest.html('');
@@ -24,18 +25,56 @@ define(
         });
 
         QUnit.test('Renders correctly', function (QUnit) {
-            createTemplate('test1', '<%= 40 + 2 %>');
+            // static template view
+            createTemplate('staticTpl', '<%= 40 + 2 %>');
 
             var el = $('<div class="js-test1"></div>').appendTo(viewNest);
 
-            var MyView = View.extend({
-                tpl: 'test1'
+            var StaticView = View.extend({
+                tpl: 'staticTpl'
             });
 
-            var view = new MyView(_.extend({}, passReg, {el: el}));
-            view.render();
+            var staticView = new StaticView(_.extend({}, passReg, {el: el}));
+            staticView.render();
 
-            QUnit.equal(el.html(), '42', 'Renders ok')
+            QUnit.equal(el.html(), '42', 'Static template');
+
+
+            // data view
+            createTemplate('dataTpl', '<%= a + b %>');
+            el = $('<div class="js-test2"></div>').appendTo(viewNest);
+
+            var DataView = View.extend({
+                    tpl: 'dataTpl'
+                }),
+                data = {
+                    a: 40,
+                    b: 2
+                };
+
+            var dataView = new DataView(_.extend({}, passReg, {el: el, model: data}));
+            dataView.render();
+
+            QUnit.equal(el.html(), '42', 'Data to template passing');
+
+
+            // model or collection view
+            createTemplate('modelTpl', '<%= a + b %>');
+            el = $('<div class="js-test3"></div>').appendTo(viewNest);
+
+            var ModelView = View.extend({
+                    tpl: 'modelTpl'
+                }),
+                model = {
+                    toTemplate: function () {
+                        return {a: 40, b: 2}
+                    }
+                };
+
+            var modelView = new ModelView(_.extend({}, passReg, {el: el, model: model}));
+            modelView.render();
+
+            QUnit.equal(el.html(), '42', 'Calling .toTemplate method');
         });
 
         viewNest.remove();
